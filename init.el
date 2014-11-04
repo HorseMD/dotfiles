708aa4285eb9c16270ed369d7b40e3de5a81931c
HorseMD <alightedness@gmail.com>
2014-11-04 12:30:00 +0000

    Fix incorrect melpa repo url

--- init.el
+++ init.el
@@ -23,9 +23,9 @@
 (require 'whitespace)
 (require 'package)
 
-(defvar package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-			   ("marmalade" . "http://marmalade-repo.org/packages/")
-			   ("melpa-stable" . "http://stable.melpa.net/packages/")))
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("melpa-stable" . "http://stable.melpa.org/packages/")))
 (package-initialize)
 
 (dolist (package package-list)
