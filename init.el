e2f097c7c842231ce7bbfc1589a21866501b2851
HorseMD <alightedness@gmail.com>
2014-11-04 12:44:52 +0000

    Remove redundant custom-set-{variables, faces}

--- init.el
+++ init.el
@@ -83,16 +83,3 @@
 (global-font-lock-mode 1)
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("bd115791a5ac6058164193164fd1245ac9dc97207783eae036f0bfc9ad9670e0" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
